e935acda874863d9f17b8531340b8b1ef67be626
simenravnik <ravnik.simen@gmail.com>
2019-12-25 13:52:27 +0100

    Update product list frontend implementation

--- views/product-list.php
+++ views/product-list.php
@@ -55,99 +55,210 @@ switch ($data["do"]) {
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Online shop">
+        <meta name="author" content="Simen Ravnik">
+
+        <title>Shop</title>
+
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-        <link rel ="stylesheet" type="text/css" href="<?= CSS_URL . "bootstrap.min.css" ?>">
-        <link rel="stylesheet" type="text/css" href="<?= CSS_URL . "style.css" ?>">
-        <meta charset="UTF-8" />
-        <title>Seznam artiklov</title>
+
+        <link rel ="stylesheet" type="text/css" href="<?= CSS_URL . "shop-homepage.css" ?>">
+        <link rel ="stylesheet" type="text/css" href="<?= CSS_URL . "bootstrap4-retro.min.css" ?>">
+
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Shrikhand" rel="stylesheet">
+
     <head>
     <body>
-        <h1>Spletna trgovina</h1>
-
-        <p>[
-        <?php if(!isset($_SESSION["loggedin"])) { ?>
-            <a href="<?= BASE_URL . "registration" ?>">Registracija</a> |
-            <a href="<?= BASE_URL . "login" ?>">Prijava za stranke</a> |
-            <a href="<?= BASE_URL . "certificate" ?>">Prijava s certifikatom</a>
-        <?php } else {
-            if($_SESSION["type"] == 1) { ?>
-                <a href="<?= BASE_URL . "products/add" ?>">Dodaj nov artikel</a> |
-                <a href="<?= BASE_URL . "users" ?>">Vsi uporabniki</a> |
-                <a href="<?= BASE_URL . "orders" ?>">Vsa naročila</a> |
-            <?php } else if ($_SESSION["type"] == 0) { ?>
-                <a href="<?= BASE_URL . "users" ?>">Vsi uporabniki</a> |
-        <?php } else if ($_SESSION["type"] == 2 ){ ?>
-                <a href="<?= BASE_URL . "orders" ?>">Vsa naročila</a> |
-        <?php } ?>
-            <a href="<?= BASE_URL . "profile/" . $_SESSION["id"] ?>">Uredi profil</a> |
-            <a href="<?= BASE_URL . "logout" ?>">Odjava</a>
-        <?php } ?>
-        ]</p>
-
-        <div id ="main">
-            <?php
-            foreach ($products as $product): ?>
-                <div class="product">
-                    <form action="<?= BASE_URL . "products" ?>" method="post">
-                        <input type="hidden" name="do" value="add_into_cart" />
-                        <input type="hidden" name="id" value="<?= $product["id"] ?>" />
-                        <p><?= $product["title"] ?></p>
-                        <b><p><?= number_format($product["price"], 2) ?> €<br/></b>
-                        <a href="<?= BASE_URL . "products/" . $product["id"] ?>" class="btn btn-info details"><i class="fas fa-info-circle"></i> Podrobnosti</a>
-                        <?php if(isset($_SESSION["loggedin"]) && $_SESSION["type"] == 2) { ?>
-                            <?php if($product["activated"] == 1) { ?>
-                                <button type="submit" class="btn btn-info add-to-cart"><i class="fas fa-cart-plus"></i> V košarico</button>
-                            <?php } else { ?>
-                                <p>Trenutno ni na voljo</p>
-                            <?php } ?>
-                        <?php } ?>
-                    </form>
-                </div>
-            <?php endforeach; ?>
-        </div>
-        <?php if(isset($_SESSION["loggedin"]) && $_SESSION["type"] == 2) { ?>
-            <div class="cart">
-                <h3>Košarica</h3>
-
-                <?php
-                $kosara = isset($_SESSION["cart"]) ? $_SESSION["cart"] : [];
-
-                if ($kosara) {
-                    $znesek = 0;
-                    foreach ($kosara as $id => $kolicina):
-                        $product = ProductDB::get(array("id" => $id));
-                        $znesek += $product["price"] * $kolicina;
-                        ?>
-                        <form action="<?= BASE_URL . "products" ?>" method="post">
-                            <input type="hidden" name="do" value="update_cart" />
-                            <input type="hidden" name="id" value="<?= $product["id"] ?>" />
-                            <input type="number" name="kolicina" value="<?= $kolicina ?>"
-                                   class="short_input" />
-                            &times; <?=
-                            (strlen($product["title"]) < 50) ?
-                                    $product["title"] :
-                                    substr($product["title"], 0, 26) . " ..."
-                            ?> (<?= number_format($product["price"], 2) ?> €)
-                            <button class="update-cart" type="submit"><i class="fas fa-sync-alt"></i> Posodobi</button>
-                        </form>
-                    <?php endforeach; ?>
-
-                    <p>Skupaj: <b><?= number_format($znesek, 2) ?> EUR</b></p>
-
-                    <form action="<?= BASE_URL . "orders/confirmation" ?>" method="POST">
-                          <button type="submit" class="btn btn-success gumb"><i class="fas fa-receipt"></i></i> Pripravi naročilo</button>
-                    </form>
-                    <form action="<?= BASE_URL . "products" ?>" method="POST">
-                        <input type="hidden" name="do" value="purge_cart" />
-                        <button type="submit" class="btn btn-danger gumb"><i class="fas fa-trash-alt"></i> Izprazni košarico</button>
-                    </form>
-
-                <?php } elseif(!isset($_SESSION["loggedin"])) { ?>
-                    Za dodajanje v košarico se je potrebno prijaviti.
-                <?php } else { ?>
-                    Košara je prazna.
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+          <div class="container">
+            <a class="navbar-brand" href="#">Online Shop</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+             <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="<?= BASE_URL . "products" ?>">Home
+                    <span class="sr-only">(current)</span>
+                  </a>
+                </li>
+                <?php if(!isset($_SESSION["loggedin"])) { ?>
+                    <li class="nav-item">
+                      <a class="nav-link" href="<?= BASE_URL . "registration" ?>">Register</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="<?= BASE_URL . "login" ?>">Login</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="<?= BASE_URL . "certificate" ?>">Admins/Sellers</a>
+                    </li>
+                <?php } else {
+                    if($_SESSION["type"] == 1) { ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "products/add" ?>">Add products</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "users" ?>">Customers</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "orders" ?>">Orders</a>
+                        </li>
+                    <?php } else if ($_SESSION["type"] == 0) { ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "users" ?>">Sellers</a>
+                        </li>
+                     <?php } else if ($_SESSION["type"] == 2 ){ ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "orders" ?>">My orders</a>
+                        </li>
+                     <?php } ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "profile/" . $_SESSION["id"] ?>">Edit profile</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "logout" ?>">Logout</a>
+                        </li>
                 <?php } ?>
+             </ul>
             </div>
-        <?php } ?>
+          </div>
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container">
+
+          <div class="row">
+
+            <div class="col-lg-3">
+
+              <h1 class="my-4">Products</h1>
+              <div class="list-group">
+                 <?php if(isset($_SESSION["loggedin"]) && $_SESSION["type"] == 2) { ?>
+                    <div class="cart">
+                        <h3>Košarica</h3>
+
+                        <?php
+                        $kosara = isset($_SESSION["cart"]) ? $_SESSION["cart"] : [];
+
+                        if ($kosara) {
+                             $znesek = 0;
+                             foreach ($kosara as $id => $kolicina):
+                                 $product = ProductDB::get(array("id" => $id));
+                                 $znesek += $product["price"] * $kolicina;
+                                 ?>
+                                 <form action="<?= BASE_URL . "products" ?>" method="post">
+                                     <input type="hidden" name="do" value="update_cart" />
+                                     <input type="hidden" name="id" value="<?= $product["id"] ?>" />
+                                     <input type="number" name="kolicina" value="<?= $kolicina ?>"
+                                           class="short_input" />
+                                     &times; <?=
+                                     (strlen($product["title"]) < 50) ?
+                                             $product["title"] :
+                                             substr($product["title"], 0, 26) . " ..."
+                                     ?> (<?= number_format($product["price"], 2) ?> €)
+                                     <button class="update-cart" type="submit"><i class="fas fa-sync-alt"></i> Posodobi</button>
+                                 </form>
+                             <?php endforeach; ?>
+
+                             <p>Skupaj: <b><?= number_format($znesek, 2) ?> EUR</b></p>
+
+                             <form action="<?= BASE_URL . "orders/confirmation" ?>" method="POST">
+                                   <button type="submit" class="btn btn-success gumb"><i class="fas fa-receipt"></i></i> Pripravi naročilo</button>
+                             </form>
+                             <form action="<?= BASE_URL . "products" ?>" method="POST">
+                                 <input type="hidden" name="do" value="purge_cart" />
+                                 <button type="submit" class="btn btn-danger gumb"><i class="fas fa-trash-alt"></i> Izprazni košarico</button>
+                             </form>
+
+                        <?php } elseif(!isset($_SESSION["loggedin"])) { ?>
+                             Za dodajanje v košarico se je potrebno prijaviti.
+                        <?php } else { ?>
+                             Košara je prazna.
+                        <?php } ?>
+                    </div>
+                 <?php } ?>
+                <a href="#" class="list-group-item">Nutrition</a>
+              </div>
+
+            </div>
+            <!-- /.col-lg-3 -->
+
+            <div class="col-lg-9">
+
+              <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                  <div class="carousel-item active">
+                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+
+              <div class="row">
+                <?php foreach ($products as $product): ?>
+                   <div class="col-lg-4 col-md-6 mb-4">
+                     <div class="card h-100">
+                       <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                       <div class="card-body">
+                         <h4 class="card-title">
+                           <a href="<?= BASE_URL . "products/" . $product["id"] ?>"><?= $product["title"] ?></a>
+                         </h4>
+                         <h5><?= number_format($product["price"], 2) ?> €</h5>
+                         <p class="card-text"><?= $product["description"] ?></p>
+                         <form action="<?= BASE_URL . "products" ?>" method="post">
+                            <input type="hidden" name="do" value="add_into_cart" />
+                            <input type="hidden" name="id" value="<?= $product["id"] ?>" />
+                            <?php if(isset($_SESSION["loggedin"]) && $_SESSION["type"] == 2) { ?>
+                                <?php if($product["activated"] == 1) { ?>
+                                    <button type="submit" class="btn btn-info add-to-cart"><i class="fas fa-cart-plus"></i> V košarico</button>
+                                <?php } else { ?>
+                                    <p>Trenutno ni na voljo</p>
+                                <?php } ?>
+                            <?php } ?>
+                        </form>
+                       </div>
+                       <div class="card-footer">
+                         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                       </div>
+                     </div>
+                   </div>
+                   <?php endforeach; ?>
+              </div>
+              <!-- /.row -->
+
+            </div>
+            <!-- /.col-lg-9 -->
+
+          </div>
+          <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     </body>
 </html>
