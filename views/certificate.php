0ec046c711c1cbb6f3c42fb969ac6e8ccb284d14
simenravnik <ravnik.simen@gmail.com>
2019-12-27 15:04:10 +0100

    Implement certificate login

--- views/certificate.php
+++ views/certificate.php
@@ -125,34 +125,133 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Prijava</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Prijava</h2>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-                <label>Email</label>
-                <input type="text" name="email" class="form-control" value="<?= $email ?>" autocomplete="off">
-                <span class="help-block"><?php echo $email_err; ?></span>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Online shop">
+        <meta name="author" content="Simen Ravnik">
+
+        <title>Shop</title>
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel ="stylesheet" type="text/css" href="<?= CSS_URL . "bootstrap4-retro.min.css" ?>">
+        <link rel ="stylesheet" type="text/css" href="<?= CSS_URL . "shop-homepage.css" ?>">
+
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Shrikhand" rel="stylesheet">
+
+    <head>
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-sm navbar-light fixed-top" style="background-color: #ffffff;">
+          <div class="container">
+            <a class="navbar-brand" href="<?= BASE_URL . "products" ?>">Online Shop</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+             <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= BASE_URL . "products" ?>">Home
+                    <span class="sr-only">(current)</span>
+                  </a>
+                </li>
+                <?php if(!isset($_SESSION["loggedin"])) { ?>
+                    <li class="nav-item">
+                      <a class="nav-link" href="<?= BASE_URL . "registration" ?>">Register</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="<?= BASE_URL . "login" ?>">Login</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" href="<?= BASE_URL . "certificate" ?>">Admins/Sellers</a>
+                    </li>
+                <?php } else {
+                    if($_SESSION["type"] == 1) { ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "products/add" ?>">Add products</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "users" ?>">Customers</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "orders" ?>">Orders</a>
+                        </li>
+                    <?php } else if ($_SESSION["type"] == 0) { ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "users" ?>">Sellers</a>
+                        </li>
+                     <?php } else if ($_SESSION["type"] == 2 ){ ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "orders" ?>">My orders</a>
+                        </li>
+                     <?php } ?>
+                        <li class="nav-item">
+                           <a class="nav-link" href="<?= BASE_URL . "profile/" . $_SESSION["id"] ?>">Edit profile</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link btn btn-logout" href="<?= BASE_URL . "logout" ?>">Logout</a>
+                        </li>
+                <?php } ?>
+             </ul>
             </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Geslo</label>
-                <input type="password" name="password" class="form-control" autocomplete="off">
-                <span class="help-block"><?php echo $password_err; ?></span>
+          </div>
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container">
+
+          <div class="row">
+
+            <div class="col-lg-3">
+              <h1 class="my-4">Login</h1>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Prijava">
+            <!-- /.col-lg-3 -->
+
+            <div class="col-lg-9">
+
+               <div class="wrapper">
+                   <br>
+                   <h2>Admins and sellers login</h2>
+                   <br>
+
+                   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                       <div class="form-group">
+                           <label>Email</label>
+                           <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?= $email ?>" autocomplete="off">
+                           <span class="help-block <?php echo (!empty($email_err)) ? 'text-danger' : ''; ?>"><?php echo $email_err; ?></span>
+                       </div>
+
+                       <div class="form-group">
+                           <label>Password</label>
+                           <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" autocomplete="off">
+                           <span class="help-block <?php echo (!empty($password_err)) ? 'text-danger' : ''; ?>"><?php echo $password_err; ?></span>
+                       </div>
+
+                       <hr class="mb-4">
+                       <div class="form-group">
+                           <input type="submit" class="btn btn-primary btn-md btn-block" value="Submit">
+                       </div>
+                   </form>
+                   <br>
+               </div>
+
             </div>
-        </form>
-    </div>
-</body>
+            <!-- /.col-lg-9 -->
+
+          </div>
+          <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    </body>
 </html>
