83e2c4305d90bc9beef67fcf28423d82765bd1d4
simenravnik <ravnik.simen@gmail.com>
2019-12-27 10:58:10 +0100

    Correct link online-shop in navbar

--- views/product-list.php
+++ views/product-list.php
@@ -74,7 +74,7 @@ switch ($data["do"]) {
         <!-- Navigation -->
         <nav class="navbar navbar-expand-sm navbar-light fixed-top" style="background-color: #ffffff;">
           <div class="container">
-            <a class="navbar-brand" href="#">Online Shop</a>
+            <a class="navbar-brand" href="<?= BASE_URL . "products" ?>">Online Shop</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
              <span class="navbar-toggler-icon"></span>
             </button>
