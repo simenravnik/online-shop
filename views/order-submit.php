f5fe7998720f95ec20710df70c623d9e56b40fb2
simenravnik <ravnik.simen@gmail.com>
2019-12-30 10:21:53 +0100

    Update orders submit

--- views/order-submit.php
+++ views/order-submit.php
@@ -1,16 +1,15 @@
 <?php
-if(isset($_SESSION["cart"])) $kosara = $_SESSION["cart"];
+   if(isset($_SESSION["cart"])) $cart = $_SESSION["cart"];
 
-// add new order, status 1 means oddano, default seller is Vanesa Godec
-$order_id = OrdersController::add(["id_user" => $_SESSION["id"], "id_seller" => 2, "status" => 1]);
-foreach($kosara as $id => $kolicina):
-    OrderProductsController::add(["id_order" => $order_id, "id_product" => $id, "amount" => $kolicina]);
-endforeach;
+   $order_id = OrdersController::add(["id_user" => $_SESSION["id"], "id_seller" => 2, "status" => 1]);
+   foreach($cart as $id => $amount):
+       OrderProductsController::add(["id_order" => $order_id, "id_product" => $id, "amount" => $amount]);
+   endforeach;
 
-// Unset the cart
-$_SESSION["cart"] = array();
+   // Unset the cart
+   $_SESSION["cart"] = array();
 
-// Redirect to login page
-header("location:". BASE_URL . "orders");
-exit;
+   // Redirect to login page
+   header("location:". BASE_URL . "orders");
+   exit;
 ?>
