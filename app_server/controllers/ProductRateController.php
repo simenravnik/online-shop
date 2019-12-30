dc0cb88da25bd1a26f4100c2d539383b6911b515
simenravnik <ravnik.simen@gmail.com>
2019-12-30 19:31:14 +0100

    Implement backend for product rating

---
+++ app_server/controllers/ProductRateController.php
@@ -0,0 +1,70 @@
+<?php
+    header('X-Frame-Options: SAMEORIGIN');
+    header('X-Content-Type-Options: nosniff');
+    header("X-XSS-Protection: 1; mode=block");
+
+    require_once("app_server/model/ProductRateDB.php");
+    require_once("ViewHelper.php");
+
+    class ProductRateController {
+
+        public static function getProductRateById($id) {
+            return ProductRateDB::get(["id_product" => $id]);
+        }
+
+        public static function add($values) {
+            $data = array_filter($values, "self::getRules");
+
+            if (self::checkValues($data)) {
+                $id = ProductRateDB::insert($data);
+            } else {
+                echo ViewHelper::redirect(BASE_URL . "products");
+            }
+        }
+
+        public static function delete($id) {
+            $data = filter_input_array(INPUT_POST, [
+                'delete_confirmation' => FILTER_REQUIRE_SCALAR
+            ]);
+
+            if (self::checkValues($data)) {
+                ProductRateDB::delete(["id" => $id]);
+                $url = BASE_URL . "products";
+            } else {
+                $url = BASE_URL . "products/details/" . $id;
+            }
+
+            ViewHelper::redirect($url);
+        }
+
+        /**
+         * Returns TRUE if given $input array contains no FALSE values
+         * @param type $input
+         * @return type
+         */
+        public static function checkValues($input) {
+            if (empty($input)) {
+                return FALSE;
+            }
+
+            $result = TRUE;
+            foreach ($input as $value) {
+                $result = $result && $value !== false;
+            }
+
+            return $result;
+        }
+
+        /**
+         * Returns an array of filtering rules for manipulation orders
+         * @return type
+         */
+        public static function getRules() {
+            return [
+                'id_product' => FILTER_SANITIZE_NUMBER_INT,
+                'num_ratings' => FILTER_SANITIZE_NUMBER_INT,
+                'rating' => FILTER_SANITIZE_NUMBER_INT
+            ];
+        }
+    }
+?>
---
+++ app_server/model/ProductRateDB.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('app_server/model/AbstractDB.php');
+
+class ProductRateDB extends AbstractDB {
+
+   public static function insert(array $params) {
+      return parent::modify("INSERT INTO rate (id_product, num_ratings, rating) "
+                       . " VALUES (:id_product, :num_ratings, :rating)", $params);
+   }
+
+   public static function update(array $params) {
+      return parent::modify("UPDATE rate SET id_product = :id_order, "
+                       . "num_ratings = :num_ratings, rating = :rating"
+                       . " WHERE id_product = :id_product", $params);
+   }
+
+   public static function delete(array $id_product) {
+      return parent::modify("DELETE FROM rate WHERE id_product = :id_product", $id_product);
+   }
+
+   public static function get(array $id_product) {
+      $all_ratings = parent::query("SELECT id_product, num_ratings, rating"
+                       . " FROM rate"
+                       . " WHERE id_product = :id_product", $id_product);
+
+      return $all_ratings;
+   }
+
+   public static function getAll() {
+      return parent::query("SELECT id_product, num_ratings, rating"
+                       . " FROM rate"
+                       . " ORDER BY id_product ASC");
+   }
+
+   public static function getAllwithURI(array $prefix) {
+      return parent::query("SELECT id_product, num_ratings, rating"
+                       . "          CONCAT(:prefix, id_product) as uri "
+                       . "FROM rate "
+                       . "ORDER BY id_product ASC", $prefix);
+   }
+
+}
