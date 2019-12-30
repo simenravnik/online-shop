89fbad5cdcce93917f140ccbbe740e67b2127119
simenravnik <ravnik.simen@gmail.com>
2019-12-30 16:04:01 +0100

    Fix typo

--- app_api/ProductsControllerREST.php
+++ app_api/ProductsControllerREST.php
@@ -4,7 +4,7 @@ header('X-Content-Type-Options: nosniff');
 header("X-XSS-Protection: 1; mode=block");
 
 require_once("app_server/model/ProductDB.php");
-require_once("capp_server/ontrollers/ProductsController.php");
+require_once("app_server/controllers/ProductsController.php");
 require_once("ViewHelper.php");
 
 class ProductsControllerREST {
