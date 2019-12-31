e2b9117adedf3bd15a39bd42a957896b079c89b0
simenravnik <ravnik.simen@gmail.com>
2019-12-31 10:07:06 +0100

    Update init db create table images

--- sql/db.sql
+++ sql/db.sql
@@ -208,3 +208,10 @@ INSERT INTO `rate` VALUES
 /*!40000 ALTER TABLE `rate` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+CREATE TABLE `images` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `image` longblob NOT NULL,
+ `created` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
