5d61aca1f96364b1aeab9299bac209ff9c780a31
simenravnik <ravnik.simen@gmail.com>
2019-12-31 09:54:20 +0100

    Implement product rating in product list

--- app_server/views/product-list.php
+++ app_server/views/product-list.php
@@ -273,7 +273,25 @@ $search = "";
                            </form>
                           </div>
                           <div class="card-footer">
-                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                            <?php  $product_rate_data = ProductRateController::getProductRateById($product["id"]); ?>
+                            <small class="text-muted">
+                               <?php if ($product_rate_data['0']['rating'] != 0)  { ?>
+                               <?php
+                                	$starNumber = (isset($product_rate_data['0']['rating'])) ? $product_rate_data['0']['rating'] : 0;
+                                	for( $x = 0; $x < 5; $x++ )
+                                    {
+                                        if( floor($starNumber)-$x >= 1 )
+                                        { echo '<i class="fa fa-star"></i>'; }
+                                        elseif( $starNumber-$x > 0 )
+                                        { echo '<i class="fa fa-star-half-alt"></i>'; }
+                                        else
+                                        { echo '<i class="fa fa-star-o"></i>'; }
+                                    }
+                                ?>
+                             <?php } else { ?>
+                                No ratings yet.
+                             <?php } ?>
+                            </small>
                           </div>
                         </div>
                       </div>
@@ -291,6 +309,7 @@ $search = "";
         <!-- /.container -->
         <br>
 
+        <script src="https://kit.fontawesome.com/42e36ef1b7.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
